aa50dcc15f76c467d739f831679856006e8e3f87
Jan Sarenik <jajomojo@gmail.com>
2020-01-29 17:42:23 +0100

    markdown-it

--- examples/chat/add-from-dist.sh
+++ examples/chat/add-from-dist.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -e
 
-npm install jquery linkifyjs
+npm install jquery linkifyjs markdown-it
 cp -v node_modules/jquery/dist/jquery.min.js public/js
 cp -v node_modules/linkifyjs/dist/linkify.min.js public/js
 cp -v node_modules/linkifyjs/dist/linkify-jquery.min.js public/js
 cp -v node_modules/linkifyjs/dist/linkify-plugin-mention.amd.js public/js
+cp -v node_modules/markdown-it/dist/markdown-it.min.js public/js/
 rm -rf node_modules
