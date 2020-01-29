d1804ecaf9128c2c587d5ae2c0a710be3e0bdf0a
Jan Sarenik <jajomojo@gmail.com>
2020-01-29 17:40:26 +0100

    index.js: Bind to localhost only

--- examples/chat/index.js
+++ examples/chat/index.js
@@ -6,7 +6,7 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
-server.listen(port, () => {
+server.listen(port, 'localhost', () => {
   console.log('Server listening at port %d', port);
 });
 
